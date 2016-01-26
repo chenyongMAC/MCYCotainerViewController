0e56b267059a3a6b95e53ef5994ace8f0230224a
chenyongMAC <290363831@qq.com>
2016-01-26 23:32:14 +0800

    add YYImageProgressiveExample files

---
+++ MCYCotainerViewController/Src/ViewControllers/YYImage/YYImageProgressiveExample.h
@@ -0,0 +1,13 @@
+//
+//  YYImageProgressiveExample.h
+//  MCYCotainerViewController
+//
+//  Created by 陈勇 on 16/1/26.
+//  Copyright © 2016年 陈勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYImageProgressiveExample : UIViewController
+
+@end
