1cfd413a9c383c21a8967fa201c24b87da8c6e80
叶伟伟 <yww7289@163.com>
2016-02-25 19:02:33 +0800

    Complete Menu view' UI

--- Oneday/Oneday/ContainerViewController.swift
+++ Oneday/Oneday/ContainerViewController.swift
@@ -8,37 +8,77 @@
 
 import UIKit
 
+let MENU_LABEL_MARGIN: CGFloat = 10
+
 class ContainerViewController: UIViewController {
 
   @IBOutlet weak var menuView: UIView!
   
+  var selectedIndexOfMenu = 0
+  let textColorForMenuLabel = UIColor(red: 0.866667, green: 0.866667, blue: 0.866667, alpha: 1.0)
+  let selectedTextColorForMenuLabel = UIColor(red: 0.333333, green: 0.333333, blue: 0.333333, alpha: 1.0)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // In StoryBoard, I set constrains, but it doesn't work
+    // So I set frames manully, Auto Layout bugs?
+    menuView.frame.size.width = view.frame.width
+    
     setupMenuViewWithTitles(["mainVC", "mainTabVC", "YYImageDemo"])
   }
+
+}
+
+// MARK: - menu view configure
+extension ContainerViewController {
   
   func setupMenuViewWithTitles(titles: [String]) {
-    let labelMargin: CGFloat = 10
     var widthOfLabels: CGFloat = 0.0
     var labels = [UILabel]()
-    var nextPositionOfLabels: CGFloat = 0.0
     for title in titles {
       let label = UILabel(frame: CGRect.zero)
       label.text = title
+      label.font = UIFont.systemFontOfSize(16)
       label.sizeToFit()
-      label.frame.size.height = 40
-      labels.append(label)
       widthOfLabels += label.frame.width
-    }
-    widthOfLabels += labelMargin * CGFloat(labels.count - 1)
-    nextPositionOfLabels = (view.bounds.width - widthOfLabels) / 2
-    for label in labels {
-      label.frame.origin.x = nextPositionOfLabels
+      labels.append(label)
       menuView.addSubview(label)
-      nextPositionOfLabels += label.frame.width + labelMargin
+    }
+    widthOfLabels += MENU_LABEL_MARGIN * CGFloat(labels.count - 1)
+    configureLabelsInMenuView(menuView, withLabelsWidth: widthOfLabels)
+  }
+  
+  func configureLabelsInMenuView(menuView: UIView, withLabelsWidth labelsWidth: CGFloat) {
+    guard let labels = menuView.subviews as? [UILabel] else{ return }
+    var nextPositonOfLabels = (menuView.bounds.width - labelsWidth) / 2
+    for (index, label) in labels.enumerate() {
+      label.textColor = textColorForMenuLabel
+      label.frame.size.height = 40
+      label.frame.origin.x = nextPositonOfLabels
+      label.userInteractionEnabled = true
+      label.tag = 2000 + index
+      let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "menuItemSwitched:")
+      label.addGestureRecognizer(tapGestureRecognizer)
+      nextPositonOfLabels += label.frame.width + MENU_LABEL_MARGIN
+    }
+    
+    // the first item of menu view
+    labels.first?.textColor = selectedTextColorForMenuLabel
+  }
+  
+  func menuItemSwitched(recognizer: UITapGestureRecognizer) {
+    guard let labels = menuView.subviews as? [UILabel] else { return }
+    guard let sender = recognizer.view as? UILabel else { return }
+    let selectedIndex = sender.tag - 2000
+    for (index, label) in labels.enumerate() {
+      if selectedIndex == index {
+        label.textColor = selectedTextColorForMenuLabel
+      }else {
+        label.textColor = textColorForMenuLabel
+      }
     }
   }
-
+  
 }
 
